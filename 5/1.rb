748cc39ceb0523b06b1814dc607ba6083b1e00c2
Jakub Hozak <jakub.hozak@gmail.com>
2019-12-06 00:06:15 +0100

    5/1 ready

--- 5/1.rb
+++ 5/1.rb
@@ -7,8 +7,13 @@ require_relative 'test_helper'
 
 
 assert_computation [ 1002,4,3,4,33 ], [ 1002,4,3,4,99 ]
-
-
 assert_computation [ 2,3,0,3,99 ],  [ 2,3,0,6,99 ]
 assert_computation [ 2,4,4,5,99,0 ], [2,4,4,5,99,9801 ]
 assert_computation [ 1,1,1,4,99,5,6,0,99 ], [ 30,1,1,4,2,5,6,0,99 ]
+
+
+diagnostics = File.new('input.txt').read.split(',').map(&:to_i)
+
+IntcodeComputer.new( diagnostics ).run([ 1 ]) do |out|
+  puts out
+end
